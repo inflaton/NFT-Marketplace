15ff2d6cb783ddbe560e2ee98c14477a21540ee0
Donghao Huang <donghao.huang@mastercard.com>
2022-11-22 11:12:00 +0800

    deployed CNFT contract

--- smart-contracts/scripts/l2-tokens.sh
+++ smart-contracts/scripts/l2-tokens.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -u
 
 echo Tokens:
-curl https://api-dev.reddio.com/v1/contracts/0x6b4f24fD500beDB945FC8b93882766BB5e7CF620/tokens
+curl https://api-dev.reddio.com/v1/contracts/0x3fB44BFa72591Ff4be8bf048384b17c2fDAf9622/tokens
 
 echo Balances:
 curl 'https://api-dev.reddio.com/v1/balances?stark_key=0x5468e035879734e63e2db83ccb953bce38096590d58aa0edde4034de0806790&page=1&limit=100' | jq
