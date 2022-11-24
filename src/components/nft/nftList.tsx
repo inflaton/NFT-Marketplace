a4cf81779997f6725f2c854b479031e0e1d6561d
Donghao Huang <donghao.huang@mastercard.com>
2022-11-24 15:29:19 +0800

    use latest code from reddio

--- src/components/nft/nftList.tsx
+++ src/components/nft/nftList.tsx
@@ -50,7 +50,8 @@ const NFTList = () => {
           ids = data.data.list
             .filter(
               (item) =>
-                item.balance_available && item.contract_address.toLowerCase() === address.toLowerCase(),
+                item.balance_available &&
+                item.contract_address.toLowerCase() === address.toLowerCase(),
             )
             .map((item) => item.token_id);
           // @ts-ignore
