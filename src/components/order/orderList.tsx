8a12424b79c1c1999e8b73ed7cab51dc93aeebb7
Donghao Huang <donghao.huang@mastercard.com>
2022-11-25 09:15:51 +0800

    debug pic display

--- src/components/order/orderList.tsx
+++ src/components/order/orderList.tsx
@@ -40,7 +40,7 @@ const OrderList = () => {
         setOrderList(arr);
 
         console.log(arr);
-        const images = arr.map((item) => ({ image: `https://nft-marketplace-l2.netlify.app/images/${item.token_id}` }));
+        const images = arr.map((item) => ({ image: `https://nft-marketplace-l2.netlify.app/images/${item.token_id}.png` }));
         console.log(images);
         // setImages(images);
         // const tokenIds = arr.map((item) => item.token_id).join(',');
